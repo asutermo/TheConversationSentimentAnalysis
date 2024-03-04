b0250b8fd1f209935f920a655d6e32a976b8e62e
asutermo <a131313.com>
2024-03-04 13:21:08 -0700

    messing with async

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import datetime
-
 from flask import Flask, render_template
 
 from scripts import basic_sentiment, test
@@ -8,9 +6,14 @@ app = Flask(__name__)
 
 # the minimal Flask application
 @app.route('/')
-def index():
-    return render_template('index.html', utc_dt=test.get_datetime(), summaries=basic_sentiment.analyze_rss_feed_titles())
+async def index():
+    summaries = await basic_sentiment.analyze_rss_feed_titles()
+    return render_template('index.html', utc_dt=test.get_datetime(), summaries=summaries)
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
--- scripts/basic_sentiment.py
+++ scripts/basic_sentiment.py
@@ -8,7 +8,7 @@ from . import TextBlobArticleSentiment
 # Set feed url, parse it, then check polarity and subjectivity of each article's title
 FEED_URL = 'https://theconversation.com/articles.atom?language=en'
 
-def analyze_rss_feed_titles() -> List[str]:
+async def analyze_rss_feed_titles() -> List[str]:
     feed = feedparser.parse(FEED_URL)
     title_blobs =  [(entry.title, TextBlob(entry.title)) for entry in feed.entries]
     return [str(TextBlobArticleSentiment(title, blob.sentiment.polarity, blob.sentiment.subjectivity)) for title, blob in title_blobs]
