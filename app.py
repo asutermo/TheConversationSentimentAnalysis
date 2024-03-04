e374acdc49d0627da03c48d82dcb34e2b1d8786d
asutermo <a131313.com>
2024-03-04 12:38:07 -0700

    test script just for verifying

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ import datetime
 
 from flask import Flask, render_template
 
+from scripts import test
 
 app = Flask(__name__)
 
 # the minimal Flask application
 @app.route('/')
 def index():
-    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
+    return render_template('index.html', utc_dt=test.get_datetime())
 
 @app.route('/about/')
 def about():
