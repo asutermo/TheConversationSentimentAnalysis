780372597f365e861ddb78769af65b6170f8f32c
asutermo <a131313.com>
2024-03-04 12:03:59 -0700

    sentiment analysis

--- webapp/app.py
+++ webapp/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 # the minimal Flask application
 @app.route('/')
 def index():
-    return '<h1>Hello, World!</h1>'
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
