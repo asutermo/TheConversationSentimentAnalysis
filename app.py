1d37c6b6fb0f8332c89f82b85eb30e7123a74051
asutermo <a131313.com>
2024-03-06 14:36:55 -0700

    article

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from scripts import basic_sentiment, test
 
@@ -14,6 +14,14 @@ async def index():
 def about():
     return render_template('about.html')
 
+@app.route('/article/<title>')
+async def article(title: str):
+    true_link = request.args.get('link')
+    summary = await basic_sentiment.analyze_article(title=title, url=true_link)
+    if summary:
+        return render_template('article.html', summary=summary)
+    else:
+        return render_template('404.html', summary=summary)
 
 if __name__ == '__main__':
     app.run()
--- scripts/basic_sentiment.py
+++ scripts/basic_sentiment.py
@@ -1,6 +1,8 @@
-import feedparser
+import requests
 from typing import List
 
+from bs4 import BeautifulSoup
+import feedparser
 from textblob import TextBlob
 
 from . import TextBlobArticleSentiment
@@ -8,9 +10,22 @@ from . import TextBlobArticleSentiment
 # Set feed url, parse it, then check polarity and subjectivity of each article's title
 FEED_URL = 'https://theconversation.com/articles.atom?language=en'
 
-async def analyze_rss_feed_titles() -> List[str]:
+async def analyze_rss_feed_titles() -> List[TextBlobArticleSentiment]:
     feed = feedparser.parse(FEED_URL)
     title_blobs =  [(entry.title, entry.link, TextBlob(entry.title)) for entry in feed.entries]
     return [(TextBlobArticleSentiment(title, link, blob.sentiment.polarity, blob.sentiment.subjectivity)) for title, link, blob in title_blobs]
 
    
+async def analyze_article(title:str, url: str) -> TextBlobArticleSentiment:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_body_html = soup.find('div', itemprop='articleBody')
+    
+    if article_body_html:
+        article_body = article_body_html.get_text(strip=True)
+    else:
+        print("The specified div was not found.")
+        return None
+    
+    blob = TextBlob(article_body)
+    return TextBlobArticleSentiment(title, url, blob.sentiment.polarity, blob.sentiment.subjectivity)
