d121030cf536b7f74e556fb81aadd627867d53bb
asutermo <a131313.com>
2024-03-06 15:08:37 -0700

    500 handling

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import Flask, abort, render_template, request
 
 from scripts import basic_sentiment, test
@@ -11,37 +9,32 @@ app = Flask(__name__)
 def not_found(e):
     return render_template('404.html'), 404
 
-@app.errorhandler(HTTPException)
-def handle_exception(e):
-    """Return JSON instead of HTML for HTTP errors."""
-    # start with the correct headers and status code from the error
-    response = e.get_response()
-    # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-    })
-    response.content_type = "application/json"
-    return response
+@app.errorhandler(500)
+def internal_server_error(e):
+    original = getattr(e, "original_exception", None)
+    return render_template("500.html"), 500
+
+    # could check original and show unhandled error
 
 @app.route('/')
 async def index():
     summaries = await basic_sentiment.analyze_rss_feed_titles()
-    return render_template('index.html', utc_dt=test.get_datetime(), summaries=summaries)
+    return render_template('index.html', utc_dt=test.get_datetime(), summaries=summaries), 200
 
 @app.route('/about/')
 def about():
-    return render_template('about.html')
+    return render_template('about.html'), 200
 
 @app.route('/article/<title>')
 async def article(title: str):
     true_link = request.args.get('link')
     summary = await basic_sentiment.analyze_article(title=title, url=true_link)
     if summary:
-        return render_template('article.html', summary=summary)
+        return render_template('article.html', summary=summary), 200
     else:
+        app.logger.info('User tried to access %s which didn\'t exist.', true_link)
         abort(404)
+
 
 if __name__ == '__main__':
     app.run()
