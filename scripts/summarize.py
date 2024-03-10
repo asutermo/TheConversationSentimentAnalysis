875d20ab1f68750687636c10f94c813a495a9a46
Andrew <andrew@forming.ai>
2024-03-09 17:45:21 -0700

    use facebook's llm

--- scripts/summarize.py
+++ scripts/summarize.py
@@ -1,9 +1,9 @@
 from transformers import pipeline
 
-# todo use transformers.js
+# todo use transformers.js and/or langchain
 def summarize_light(article_text: str) -> str:
     """Uses a fine-tuned T5 small for text summaries"""
-    summarizer = pipeline("summarization", model="Falconsai/text_summarization")
-    results = summarizer(article_text[:512], max_length=512, min_length=30, do_sample=False)
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    results = summarizer(article_text[:1024], max_length=1024, min_length=30, do_sample=False)
     print(results)
     return results[0]['summary_text']
