a9767819f5dc097076bd96f5d4015977b19b9a2b
asutermo <a131313.com>
2024-03-11 14:03:41 -0600

    refactor, work on loading model ahead of time

--- app/app.py
+++ app/app.py
@@ -1,10 +1,59 @@
+from dataclasses import dataclass
+import requests
+from typing import List
+
+from bs4 import BeautifulSoup
+import feedparser
+
 from quart import Quart, abort, render_template, request
 from quart_schema import QuartSchema
-
-from scripts import basic_sentiment
+from textblob import TextBlob
+from transformers import pipeline
 
 app = Quart(__name__)
 QuartSchema(app)
+
+@dataclass
+class ArticleSentiment:
+    title: str
+    link: str
+    polarity: float
+    subjectivity: float
+    text: str = None
+    summarization: str = None
+
+    def __str__(self):
+        return f"{self.title} - Polarity: {self.polarity}, Subjectivity: {self.subjectivity}"
+
+FEED_URL = 'https://theconversation.com/articles.atom?language=en'
+SUMMARIZER = pipeline("summarization", model="facebook/bart-large-cnn")
+
+async def analyze_rss_feed_titles() -> List[ArticleSentiment]:
+    """Takes an RSS feed, checks title for subjectivity and polarity and returns a list of ArticleSentiments"""
+    feed = feedparser.parse(FEED_URL)
+    title_blobs =  [(entry.title, entry.link, TextBlob(entry.title)) for entry in feed.entries]
+    return [(ArticleSentiment(title, link, blob.sentiment.polarity, blob.sentiment.subjectivity)) for title, link, blob in title_blobs]
+
+async def analyze_article(title:str, url: str) -> ArticleSentiment:
+    """Takes an article from The Conversation, grabs article text, and does both TextBlob and T5 analysis on it"""
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    article_body_html = soup.find('div', itemprop='articleBody')
+    
+    if article_body_html:
+        article_body = article_body_html.get_text(strip=False)
+    else:
+        return None
+    
+    blob = TextBlob(article_body)
+    summary = summarize_light(article_body)
+    return ArticleSentiment(title, url, blob.sentiment.polarity, blob.sentiment.subjectivity, article_body, summary)
+
+# Could use transformers.js for heavy lifting, but for now the model is loaded on launch
+def summarize_light(article_text: str) -> str:
+    """Uses FB Bart Large for text summaries"""
+    results = SUMMARIZER(article_text[:1024], max_length=1024, min_length=30, do_sample=False)
+    return results[0]['summary_text']
 
 @app.errorhandler(404)
 async def not_found(e):
@@ -22,7 +71,7 @@ async def internal_server_error(e):
 @app.route('/')
 async def index():
     """Just show article titles, and polarity/subjectivity"""
-    summaries = await basic_sentiment.analyze_rss_feed_titles()
+    summaries = await analyze_rss_feed_titles()
     return await render_template('index.html', summaries=summaries), 200
 
 @app.route('/about/')
@@ -34,7 +83,7 @@ async def about():
 async def article(title: str):
     """Take an article, and do a deep dive on the content."""
     true_link = request.args.get('link')
-    summary = await basic_sentiment.analyze_article(title=title, url=true_link)
+    summary = await analyze_article(title=title, url=true_link)
     if summary:
         return await render_template('article.html', summary=summary), 200
     else:
--- app/scripts/summarize.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-# todo use transformers.js and/or langchain
-def summarize_light(article_text: str) -> str:
-    """Uses a fine-tuned T5 small for text summaries"""
-    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    results = summarizer(article_text[:1024], max_length=1024, min_length=30, do_sample=False)
-    print(results)
-    return results[0]['summary_text']
