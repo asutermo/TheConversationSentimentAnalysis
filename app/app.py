6ab660822c903f24d0b3ee6f87a264b855562dd3
asutermo <a131313.com>
2024-03-11 19:34:01 -0600

    black and isort

--- app/app.py
+++ app/app.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass, asdict
-import requests
+from dataclasses import asdict, dataclass
 from typing import List
 
-from bs4 import BeautifulSoup
 import feedparser
-
+import requests
+from bs4 import BeautifulSoup
 from quart import Quart, render_template, websocket
 from quart_schema import QuartSchema
 from textblob import TextBlob
@@ -30,7 +29,7 @@ class ArticleSummarizationRequest:
     url: str
 
 
-FEED_URL = 'https://theconversation.com/articles.atom?language=en'
+FEED_URL = "https://theconversation.com/articles.atom?language=en"
 
 
 SUMMARIZER = pipeline("summarization", model="facebook/bart-large-cnn")
@@ -39,15 +38,26 @@ SUMMARIZER = pipeline("summarization", model="facebook/bart-large-cnn")
 async def analyze_rss_feed_titles() -> List[ArticleSentiment]:
     """Takes an RSS feed, checks title for subjectivity and polarity and returns a list of ArticleSentiments"""
     feed = feedparser.parse(FEED_URL)
-    title_blobs = [(entry.title, entry.link, TextBlob(entry.title)) for entry in feed.entries]
-    return [(asdict(ArticleSentiment(title, link, blob.sentiment.polarity, blob.sentiment.subjectivity))) for title, link, blob in title_blobs]
+    title_blobs = [
+        (entry.title, entry.link, TextBlob(entry.title)) for entry in feed.entries
+    ]
+    return [
+        (
+            asdict(
+                ArticleSentiment(
+                    title, link, blob.sentiment.polarity, blob.sentiment.subjectivity
+                )
+            )
+        )
+        for title, link, blob in title_blobs
+    ]
 
 
 async def analyze_article(title: str, url: str) -> ArticleSentiment:
     """Takes an article from The Conversation, grabs article text, and does both TextBlob and T5 analysis on it"""
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    article_body_html = soup.find('div', itemprop='articleBody')
+    soup = BeautifulSoup(response.text, "html.parser")
+    article_body_html = soup.find("div", itemprop="articleBody")
 
     if article_body_html:
         article_body = article_body_html.get_text(strip=False)
@@ -56,23 +66,32 @@ async def analyze_article(title: str, url: str) -> ArticleSentiment:
 
     blob = TextBlob(article_body)
     summary = summarize_light(article_body)
-    return ArticleSentiment(title, url, blob.sentiment.polarity, blob.sentiment.subjectivity, article_body, summary)
+    return ArticleSentiment(
+        title,
+        url,
+        blob.sentiment.polarity,
+        blob.sentiment.subjectivity,
+        article_body,
+        summary,
+    )
 
 
 # Could use transformers.js for heavy lifting, but for now the model is loaded on launch
 def summarize_light(article_text: str) -> str:
     """Uses FB Bart Large for text summaries"""
     art_len = len(article_text)
-    article_text_trim = article_text[:min(art_len, 1024)]
-    results = SUMMARIZER(article_text_trim, max_length=min(art_len, 1024), min_length=30, do_sample=False)
+    article_text_trim = article_text[: min(art_len, 1024)]
+    results = SUMMARIZER(
+        article_text_trim, max_length=min(art_len, 1024), min_length=30, do_sample=False
+    )
     entry = results[0]
-    return entry['summary_text']
+    return entry["summary_text"]
 
 
 @app.errorhandler(404)
 async def not_found(e):
     """This will 'catch' 404's and show a page not found."""
-    return await render_template('404.html'), 404
+    return await render_template("404.html"), 404
 
 
 @app.errorhandler(500)
@@ -84,31 +103,31 @@ async def internal_server_error(e):
     # could check original and show unhandled error
 
 
-@app.route('/')
+@app.route("/")
 async def index():
     """Just show base index page. /ws/feed is responsible for populating"""
-    return await render_template('index.html'), 200
+    return await render_template("index.html"), 200
 
 
-@app.route('/about/')
+@app.route("/about/")
 async def about():
     """Basic about page"""
-    return await render_template('about.html'), 200
+    return await render_template("about.html"), 200
 
 
-@app.route('/article/<title>')
+@app.route("/article/<title>")
 async def article(title: str):
     """Take an article, and do a deep dive on the content."""
-    return await render_template('article.html'), 200
+    return await render_template("article.html"), 200
 
 
-@app.websocket('/ws/feed')
+@app.websocket("/ws/feed")
 async def feed() -> None:
     summaries = await analyze_rss_feed_titles()
     await websocket.send_as(summaries, List[ArticleSentiment])
 
 
-@app.websocket('/ws/summarize')
+@app.websocket("/ws/summarize")
 async def summarize() -> None:
     while True:
         data = await websocket.receive_as(ArticleSummarizationRequest)
@@ -116,5 +135,5 @@ async def summarize() -> None:
         await websocket.send_as(summary, ArticleSentiment)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,26 @@
 import pytest
+
 from ..app.app import app
 
 
 @pytest.mark.asyncio
 async def test_index():
     test_client = app.test_client()
-    response = await test_client.get('/')
+    response = await test_client.get("/")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_about():
     test_client = app.test_client()
-    response = await test_client.get('/about/')
+    response = await test_client.get("/about/")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_404():
     test_client = app.test_client()
-    response = await test_client.get('/nonexistentroute/')
+    response = await test_client.get("/nonexistentroute/")
     assert response.status_code == 404
 
 
@@ -27,12 +28,12 @@ async def test_404():
 async def test_article_with_valid_link():
     test_client = app.test_client()
     # Assuming 'example_title' and 'example_link' are valid for your application
-    response = await test_client.get('/article/example_title?link=example_link')
+    response = await test_client.get("/article/example_title?link=example_link")
     assert response.status_code == 200
 
 
 @pytest.mark.asyncio
 async def test_article_with_invalid_link():
     test_client = app.test_client()
-    response = await test_client.get('/article/invalid_title')
+    response = await test_client.get("/article/invalid_title")
     assert response.status_code == 404
